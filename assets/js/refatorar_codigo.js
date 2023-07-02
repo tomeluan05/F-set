7e8022bb1d530df8832bf6c3d9ce4cd1ce50d490
Tomé Luan <tomeluan@MacBook-Pro-de-Tome.local>
2023-07-02 12:00:01 -0300

    ajustando as imagens do carousel e dos banners

--- assets/js/refatorar_codigo.js
+++ assets/js/refatorar_codigo.js
@@ -1,16 +1,16 @@
 let dados_modal = [
-    {id: 'modalPrincipal1', caminho_imagem: './assets/images/imagens_carousel/volks_banner.jpeg'},
-    {id: 'modalPrincipal2', caminho_imagem: './assets/images/imagens_carousel/uptaste_banner.jpeg'},
-    {id: 'modalPrincipal3', caminho_imagem: './assets/images/imagens_carousel/tektrade_banner.jpeg'},
-    {id: 'modalPrincipal4', caminho_imagem: './assets/images/imagens_carousel/ge_banner.jpeg'},
-    {id: 'modalPrincipal5', caminho_imagem: './assets/images/imagens_carousel/intermarine_banner.jpeg'},
-    {id: 'modalPrincipal6', caminho_imagem: './assets/images//imagens_carousel/adeste_banner.jpeg'},
-    {id: 'modalPrincipal7', caminho_imagem: './assets/images/imagens_carousel/nortel_banner.jpeg'},
-    {id: 'modalPrincipal8', caminho_imagem: './assets/images/imagens_carousel/blink_banner.jpeg'},
-    {id: 'modalPrincipal9', caminho_imagem: './assets/images/imagens_carousel/anna_banner.jpeg'},
-    {id: 'modalPrincipal10', caminho_imagem: './assets/images/imagens_carousel/feira_banner.png'},
-    {id: 'modalPrincipal11', caminho_imagem: './assets/images/imagens_carousel/kanechom_banner.png'},
-    {id: 'modalPrincipal12', caminho_imagem: './assets/images/imagens_carousel/feira_banner.png'},
+    {id: 'modalPrincipal1', titulo: 'Uptaste', caminho_imagem: './assets/images/imagens_carousel/uptaste_banner.jpeg'},
+    {id: 'modalPrincipal2', titulo: 'Adeste', caminho_imagem: './assets/images/imagens_carousel/adeste_banner.jpeg'},
+    {id: 'modalPrincipal3', titulo: 'Anna Pegova', caminho_imagem: './assets/images/imagens_carousel/anna_banner.jpeg'},
+    {id: 'modalPrincipal4', titulo: 'Nortel', caminho_imagem: './assets/images/imagens_carousel/nortel_banner.jpeg'},
+    {id: 'modalPrincipal5', titulo: 'Intermarine', caminho_imagem: './assets/images/imagens_carousel/intermarine_banner.jpeg'},
+    {id: 'modalPrincipal6', titulo: 'Blink', caminho_imagem: './assets/images//imagens_carousel/blink_banner.jpeg'},
+    {id: 'modalPrincipal7', titulo: 'Ge', caminho_imagem: './assets/images/imagens_carousel/ge_banner.jpeg'},
+    {id: 'modalPrincipal8', titulo: 'Volkswagem SUV', caminho_imagem: './assets/images/imagens_carousel/suv_banner.png'},
+    {id: 'modalPrincipal9', titulo: 'Volkswagem Salão', caminho_imagem: './assets/images/imagens_carousel/salao_banner.png'},
+    {id: 'modalPrincipal10', titulo: 'Volkswagem Híbrido', caminho_imagem: './assets/images/imagens_carousel/volks_banner.jpeg'},
+    {id: 'modalPrincipal11', titulo: 'Volkswagem Delivery', caminho_imagem: './assets/images/imagens_carousel/caminhao_azul_banner.jpg'},
+    {id: 'modalPrincipal12', titulo: 'Volkswagem Caminhões e Ônibus', caminho_imagem: './assets/images/imagens_carousel/feira_banner.png'},
 ]
 
 // Criando o modal desktop
@@ -24,7 +24,7 @@ dados_modal.forEach((elemento)=>{
     <div class="modal-dialog modal-dialog-centered d-flex justify-content-center">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="modalPrincipalLabel"></h5>
+          <h3 class="modal-title" id="modalPrincipalLabel">${elemento.titulo}</h3>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
         </div>
         <div class="modal-body">
